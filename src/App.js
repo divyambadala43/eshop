818adea079dca6b0e0380e9a9710530546b6c297
Divya <divyambadala43@gmail.com>
2022-04-06 00:21:46 +0530

    ADD: Home.js

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
-import Navbar from "./components/Navbar"
-import './App.css';
+import Navbar from "./components/Navbar";
+import "./App.css";
+import Home from "./components/Home";
 
 function App() {
   return (
     <>
-    <Navbar />
+      <Navbar />
+      <Home />
     </>
   );
 }
