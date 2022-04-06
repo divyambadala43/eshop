566fab8a40785da145473d29eec35a1a07e31e1e
Divya <divyambadala43@gmail.com>
2022-04-06 13:51:04 +0530

    ADD: Cart number update functionality

--- src/redux/action/index.js
+++ src/redux/action/index.js
@@ -14,33 +14,3 @@ export const delCart = (product) => {
   };
 };
 
-// case "ADDITEM":
-//       //check if product already exists
-//       const exist = state.find((x) => x.id === product.id);
-//       if (exist) {
-//         // increasing the quantity
-//         return state.map((x) =>
-//           x.id === product.id ? { ...x, qty: x.qty+1 } : x
-//         );
-//       } else {
-//         const product = action.payload;
-//         return [
-//           ...state,
-//           {
-//             ...product,
-//             gty: 1,
-//           },
-//         ];
-//       }
-//       break;
-
-//     case "DELITEM":
-//       const exist1 = state.find((x) => x.id === product.id);
-//       if (exist1.qty === 1) {
-//         return state.filter((x) => x.id !== exist1.id);
-//       } else {
-//         return state.map((x) =>
-//           x.id === product.id ? { ...x, qty: x.qty-1 } : x
-//         );
-//       }
-//       break;
