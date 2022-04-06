60f53f87231e2b71439fd78981477f3546e3a141
Divya <divyambadala43@gmail.com>
2022-04-06 10:13:50 +0530

    ADD: configure react router

--- src/App.js
+++ src/App.js
@@ -1,12 +1,17 @@
 import Navbar from "./components/Navbar";
 import "./App.css";
 import Home from "./components/Home";
+import { Routes, Route } from "react-router-dom";
+import Products from "./components/Products";
 
 function App() {
   return (
     <>
       <Navbar />
-      <Home />
+      <Routes>
+        <Route exact path="/" element={<Home />} />
+        <Route exact path="/products" element={<Products />} />
+      </Routes>
     </>
   );
 }
