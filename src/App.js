e984b33557a5ddcd2b4f04b58e0b8db0d97de38e
Divya <divyambadala43@gmail.com>
2022-04-06 11:28:11 +0530

    ADD: Product.js && FIX: Loading Skeleton issue

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import "./App.css";
 import Home from "./components/Home";
 import { Routes, Route } from "react-router-dom";
 import Products from "./components/Products";
+import Product from "./components/Product";
 
 function App() {
   return (
@@ -11,6 +12,7 @@ function App() {
       <Routes>
         <Route exact path="/" element={<Home />} />
         <Route exact path="/products" element={<Products />} />
+        <Route exact path="/products/:id" element={<Product />} />
       </Routes>
     </>
   );
